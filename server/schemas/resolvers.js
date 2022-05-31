64d27c2910242c559e251d789023f09a9ac3be46
lrodenyoder <rodenlau000@gmail.com>
2022-05-30 20:38:42 -0500

    add typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { User, Thought } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+
+    },
+    Mutation: {
+
+    }
+};
+
+module.exports = resolvers;
