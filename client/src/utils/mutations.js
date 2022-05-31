4865e8f8a17614f31d2222d0fb720485ee2015db
lrodenyoder <rodenlau000@gmail.com>
2022-05-30 21:37:02 -0500

    add utils mutations/ finalize backend changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,49 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-
+    mutation login($email: String!, password: String!){
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
 `;
 
 export const ADD_USER = gql`
-
+    mutation createUser($username: String!, email: String!, password: String!){
+        createUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
 `;
 
 export const SAVE_BOOK = gql`
-
+    mutation saveBook($bookId: String!){
+        saveBook(bookId: $bookId){
+            bookId
+            authors
+            description
+            image
+            link
+            title
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-
+  mutation deleteBook($bookId: String!) {
+    deleteBook(bookId: $bookId) {
+      user {
+          username
+          savedBooks
+      }
+    }
+  }
 `;
